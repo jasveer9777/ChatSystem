264416fc7f88c6b25eb84f39e9d3088bd5c7e3f2
jasveer <jasveer9777@gmail.com>
2026-09-10 16:15:10 +0530

    fixed domain cors

--- chatBE/app/main.py
+++ chatBE/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Chat System API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://chat-system-git-master-jp-b615.vercel.app/login"],
+    allow_origins=["http://localhost:3000","https://chat-system-git-master-jp-b615.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
